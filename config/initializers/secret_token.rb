8e3924d069ca3b6b28a172915215a9738fdb9265
zhu zhe <zhuzheiswho@gmail.com>
2013-11-06 00:35:03 +0800

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lottery::Application.config.secret_key_base = 'd0b3ca2f2bf6e9ef4c7bd6e100e3dc1804f657335a0c974cd2f2f0f255c21f0cb28240f7699159530b8ee0795fe3d635b622ab1ad6dc54d2dad9cf4de0efb0df'
